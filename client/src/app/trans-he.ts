872d05e253296815135d900dae1451f206de5d88
fleish80 <Fleish.D@gmail.com>
2018-12-31 19:30:49 +0200

    add trans

--- client/src/app/trans-he.ts
+++ client/src/app/trans-he.ts
@@ -1,4 +1,6 @@
-export const trans = {
-    'username': 'שם משתמש',
-    'password': 'סיסמא'
-}
+export const trans = ({
+    username: 'שם משתמש',
+    password: 'סיסמא',
+    usernameError: 'חובה להזין שם משתמש',
+    passwordError: 'חובה להזין סיסמא'
+});
