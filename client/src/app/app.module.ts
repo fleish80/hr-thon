6c9e713200bee5bc4c45f42556e47b7df4f766b5
fleish80 <Fleish.D@gmail.com>
2019-01-02 21:30:38 +0200

    app module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateModule } from '@ngx-translate/core';
-// import { AngularFireAuth } from '@angular/fire/auth';
+import { firebase } from './../environments/firebase';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MaterialModule } from './material.module';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { firebase } from './../environments/firebase';
-import { AngularFireAuth } from '@angular/fire/auth';
 
 
 @NgModule({
@@ -29,7 +28,6 @@ import { AngularFireAuth } from '@angular/fire/auth';
     TranslateModule.forRoot(),
     MaterialModule,
     AngularFireModule.initializeApp(firebase)
-    // AngularFireDatabaseModule,
   ],
   providers: [AngularFireAuth],
   bootstrap: [AppComponent]
