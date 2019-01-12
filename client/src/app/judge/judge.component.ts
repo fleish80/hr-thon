52f958b4df31c8006ed52f94bf3025739cc0d011
fleish80 <Fleish.D@gmail.com>
2019-01-12 18:34:22 +0200

    arrange a little bit

--- client/src/app/judge/judge.component.ts
+++ client/src/app/judge/judge.component.ts
@@ -15,11 +15,6 @@ import { SnackBarService } from '../snack-bar.service';
 export class JudgeComponent implements OnInit {
   uid: string;
   values$: Observable<any>;
-  clausesMap: Map<number, Observable<Clause[]>> = new Map<
-    number,
-    Observable<Clause[]>
-  >();
-  projectLoading: number;
 
   constructor(
     private activeRoute: ActivatedRoute,
@@ -34,16 +29,11 @@ export class JudgeComponent implements OnInit {
       if (this.uid) {
         const judge$ = this.firebaseService.getJudge(data.uid);
         const projects$ = this.firebaseService.getProjects();
-        const clauses$ = this.firebaseService.getClauses();
-        this.values$ = combineLatest(judge$, projects$, clauses$).pipe(
-          map(([judge, projects, clauses]) => ({
+        this.values$ = combineLatest(judge$, projects$).pipe(
+          map(([judge, projects]) => ({
             judge,
-            projects,
-            clauses
+            projects
           })),
-          tap(value => {
-            this.setClausesMap(value.judge, value.projects, value.clauses);
-          }),
           catchError(error => {
             console.error(error);
             this.snackBarService.openFailure();
@@ -52,56 +42,5 @@ export class JudgeComponent implements OnInit {
         );
       }
     });
-  }
-
-  setClausesMap(judge, projects, clauses) {
-    const hastProjects = judge.hasProjects;
-    for (const project of projects) {
-      if (hastProjects && hastProjects.includes(project.id)) {
-        const clauses$ = this.firebaseService.getClausesByJudgeAndProject(
-          judge,
-          project
-        );
-        this.clausesMap.set(project.id, clauses$);
-      } else {
-        this.clausesMap.set(project.id, of(clauses));
-      }
-    }
-  }
-
-  async updateRating(judge, project: Project, clauses: Clause[]) {
-    try {
-      this.projectLoading = project.id;
-      await this.firebaseService.setRating(judge, project, clauses);
-      await this.firebaseService.updateHasProject(judge, project);
-      await this.firebaseService.setProjectAvg(judge, project, clauses);
-      this.firebaseService.setSummary(project).subscribe(
-        //   data => {
-        //   console.log('data', data);
-        //   this.projectLoading = null;
-        // },
-        async (promise: Promise<any>) => {
-          try {
-            await promise;
-            this.projectLoading = null;
-            const message = this.translateService.instant('projectRegistred', {
-              projectName: project.desc
-            });
-            this.snackBarService.openSuccess(message);
-          } catch (error) {
-            this.catchUpdateRatingError(error);
-          }
-        },
-        error => this.catchUpdateRatingError(error)
-      );
-    } catch (error) {
-      this.catchUpdateRatingError(error);
-    }
-  }
-
-  private catchUpdateRatingError(error) {
-    console.error(error);
-    this.snackBarService.openFailure();
-    this.projectLoading = null;
   }
 }
--- client/src/app/judge/rating/rating.component.ts
+++ client/src/app/judge/rating/rating.component.ts
@@ -1,25 +1,43 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { FirebaseService } from './../../firebase.service';
+import { ChangeDetectionStrategy, Component, Input, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Clause } from 'src/app/clause';
+import { Judge } from 'src/app/judge';
+import { Subscriber } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { SnackBarService } from 'src/app/snack-bar.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-rating',
   templateUrl: './rating.component.html',
   styleUrls: ['./rating.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RatingComponent implements OnInit {
+export class RatingComponent implements OnInit, OnDestroy {
 
-  @Input() clauses: Clause[];
-  @Output() update = new EventEmitter<Clause[]>();
+  @Input() judge: Judge;
+  @Input() project: Project;
+  clauses: Clause[];
   ctrlMap: Map<string, FormControl> = new Map<string, FormControl>();
   form: FormGroup;
+  loading = false;
+  subscriber = new Subscriber();
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+    private firebaseService: FirebaseService,
+    private snackBarService: SnackBarService,
+    private translateService: TranslateService) { }
 
   ngOnInit() {
+    this.setClauses();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriber.unsubscribe();
+  }
+
+  private initForm() {
     this.form = this.formBuilder.group({});
-    if (this.clauses) {
       this.clauses.forEach((clause: Clause) => {
         const formControl = new FormControl(clause.rating || 0,
           [Validators.required,
@@ -29,15 +47,64 @@ export class RatingComponent implements OnInit {
         this.form.addControl(clause.title, formControl);
       });
     }
+
+  private setClauses() {
+    const hastProjects = this.judge.hasProjects;
+    let clauses$;
+    if (hastProjects && hastProjects.includes(this.project.id)) {
+      clauses$ = this.firebaseService
+      .getClausesByJudgeAndProject(this.judge, this.project);
+    } else {
+      clauses$ = this.firebaseService.getClauses();
+    }
+    this.subscriber.add(
+    clauses$.pipe(
+      finalize(() => this.loading = false)
+    ).subscribe((clauses: Clause[]) => {
+      this.clauses = clauses;
+      this.initForm();
+    },
+    (error: any) => {
+      this.catchUpdateRatingError(error);
+    }));
   }
 
-  submit() {
+  async submit() {
     if (this.form.valid) {
-      this.clauses.forEach((clause: Clause) => {
-        clause.rating = this.form.value[clause.title];
-      });
-      this.update.emit(this.clauses);
+    this.clauses.forEach((clause: Clause) => {
+      clause.rating = this.form.value[clause.title];
+    });
+    try {
+      await this.firebaseService.setRating(this.judge, this.project, this.clauses);
+      await this.firebaseService.updateHasProject(this.judge, this.project);
+      await this.firebaseService.setProjectAvg(this.judge, this.project, this.clauses);
+      this.loading = true;
+      this.subscriber.add(
+      this.firebaseService.setSummary(this.project).pipe(
+        finalize(() => this.loading = false)
+      ).subscribe(
+        async (promise: Promise<any>) => {
+          try {
+            await promise;
+            const message = this.translateService.instant('projectRegistred', {
+              projectName: this.project.desc
+            });
+            this.snackBarService.openSuccess(message);
+          } catch (error) {
+            this.catchUpdateRatingError(error);
+          }
+        },
+        error => this.catchUpdateRatingError(error)
+      ));
+    } catch (error) {
+      this.catchUpdateRatingError(error);
     }
+  }
+}
+
+  private catchUpdateRatingError(error) {
+    console.error(error);
+    this.snackBarService.openFailure();
   }
 
 }
