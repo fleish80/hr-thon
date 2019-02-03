8bc9e818499cc2d0204b68846a346800a46341b2
fleish80 <Fleish.D@gmail.com>
2019-02-03 16:48:09 +0200

    ng lint

--- client/src/environments/firebase.ts
+++ client/src/environments/firebase.ts
@@ -1,8 +1,8 @@
 export const firebase = {
-    apiKey: "AIzaSyB-aeH-cZ2KH16iKJ04OKEKTk0SAdko1oo",
-    authDomain: "hr-thon.firebaseapp.com",
-    databaseURL: "https://hr-thon.firebaseio.com",
-    projectId: "hr-thon",
-    storageBucket: "hr-thon.appspot.com",
-    messagingSenderId: "204637225264"
-  }
+  apiKey: 'AIzaSyB-aeH-cZ2KH16iKJ04OKEKTk0SAdko1oo',
+  authDomain: 'hr-thon.firebaseapp.com',
+  databaseURL: 'https://hr-thon.firebaseio.com',
+  projectId: 'hr-thon',
+  storageBucket: 'hr-thon.appspot.com',
+  messagingSenderId: '204637225264'
+};
